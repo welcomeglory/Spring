f11a8a2020944e8e93a5a17e66eb05414e12d632
welcomeglory <godbu2021@naver.com>
2024-04-02 09:37:28 +0900

    Add files via upload

---
+++ java_sjy_anno/src/main/java/edu/ex/MainClass.java
@@ -0,0 +1,30 @@
+package edu.ex;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+
+public class MainClass{
+	public static void main(String[] args) {
+		//Annotation전용 스프링 생성
+		AbstractApplicationContext ctx = 
+				new AnnotationConfigApplicationContext(ApplicationConfig.class);
+		
+		Student student1 = ctx.getBean("student1",Student.class);
+		System.out.println("이름 : "+student1.getName() );
+		System.out.println("나이 : "+student1.getAge() );
+		System.out.println("취미 :" +student1.getHobbys());
+		System.out.println("키 : "+student1.getHeight());
+		System.out.println("몸무게 : "+student1.getWeight());
+		
+		Student student2 = (Student)ctx.getBean("student2");
+		System.out.println("이름 : "+student2.getName() );
+		System.out.println("나이 : "+student2.getAge() );
+		System.out.println("취미 :" +student2.getHobbys());
+		System.out.println("키 : "+student2.getHeight());
+		System.out.println("몸무게 : "+student2.getWeight());
+
+
+
+
+	}	
+}
